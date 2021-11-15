b5814498d7e84291fdc2a56a2e3d7611810b7a62
jinhwansuh <nick02190219@gmail.com>
2021-11-16 00:42:42 +0900

    아이템 24

--- TypeInference.ts
+++ TypeInference.ts
@@ -28,7 +28,7 @@ const id3 = '123-234-123';
  */
 
 // 아이템 21
-
+/* 
 const mixed = ['x', 1];
 const v = {
   x: 1,
@@ -37,3 +37,33 @@ v.x = 3;
 v.x = '3';
 v.y = 4;
 // 위코드는 자바스크립트에서는 오류가 아니지만 타입스크립트에서는 오류다!
+ */
+
+// 아이템 22
+
+interface Coordinate {
+  x: number;
+  y: number;
+}
+
+interface BoundingBox {
+  x: [number, number];
+  y: [number, number];
+}
+
+interface Polygon {
+  exterior: Coordinate[];
+  holes: Coordinate[][];
+  bbox?: BoundingBox;
+}
+
+function isPointInPolygon(polygon: Polygon, pt: Coordinate) {
+  const box = polygon.bbox;
+  if (polygon.bbox) {
+    if (pt.x < box.x[0] || pt.x > box.x[1]) {
+      // box undefined가 나온다.
+    }
+  }
+}
+
+const getNumber = async () => 42;
